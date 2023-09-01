d0910959480a3a6e8fdb56a1fa9bf434dd3599e8
jonathanrib3 <jonathanrib.3@gmail.com>
2023-09-01 11:19:43 -0300

    Add task 1 scopes to Student model along with tests

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,4 +7,35 @@ class Student < ApplicationRecord
 
   has_many :enrollments, dependent: :destroy
   has_many :grades, through: :enrollments
+
+  scope :find_all_students_enrolled_on_year, lambda { |year|
+    Course.joins(
+      "JOIN \"enrollments\" \"e\"
+      ON \"courses\".\"id\" = \"e\".\"course_id\"
+      AND \"courses\".\"year\" = #{year}
+      RIGHT JOIN \"students\" \"s\"
+      ON \"s\".\"id\" = \"e\".\"student_id\""
+    ).order('s.id ASC')
+          .pluck('s.id', 's.name', 'e.code', 'courses.name', 'courses.year')
+  }
+
+  scope :find_student_by_id_enrolled_on_year, lambda { |year:, id:|
+    includes(enrollments: :course).where(id:, course: { year: })
+  }
+
+  scope :find_student_grades_averages_by_id, lambda { |id|
+    joins("JOIN \"enrollments\" \"e\"
+      ON \"students\".\"id\" = \"e\".\"student_id\"
+      JOIN \"grades\" \"g\"
+      ON \"e\".\"code\" = \"g\".\"enrollment_code\"
+      JOIN \"exams\" \"ex\"
+      ON \"g\".\"exam_id\" = \"ex\".\"id\"
+      JOIN \"courses\" \"c\"
+      ON \"c\".\"id\" = \"ex\".\"course_id\"
+      JOIN \"subjects\" \"s\"
+      ON \"s\".\"id\" = \"ex\".\"subject_id\"
+      WHERE \"students\".\"id\" = \"#{id}\"")
+      .group('s.name', 'c.name', 'c.year')
+      .average('g.value')
+  }
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,3 +1,4 @@
+# rubocop:disable RSpec/MultipleMemoizedHelpers
 # frozen_string_literal: true
 
 require 'rails_helper'
@@ -38,5 +39,117 @@ RSpec.describe Student do
   describe 'associations' do
     it { is_expected.to have_many(:enrollments).dependent(:destroy) }
     it { is_expected.to have_many(:grades).through(:enrollments) }
+  end
+
+  describe 'scopes' do
+    describe 'find_all_students_enrolled_on_year' do
+      context 'when searching for students enrolled on a year' do
+        let!(:year) { rand(2012..2023) }
+        let!(:course) { create(:course, { year: }) }
+        let!(:student) { create(:student) }
+        let!(:enrollment) { create(:enrollment, { course:, student: }) }
+
+        it 'retrieves all students data if they are enrolled on the specified year, containing its
+            id, name, enrollment code, course name and course year' do
+          expect(
+            described_class.find_all_students_enrolled_on_year(year)
+          ).to eq([[student.id, student.name,
+                    enrollment.code, course.name, course.year]])
+        end
+      end
+
+      context 'when a student is not enrolled on the requested year' do
+        let!(:year) { 2023 }
+        let!(:course) { create(:course, { year: 2012 }) }
+        let!(:student) { create(:student) }
+        let!(:enrollment) { create(:enrollment, { course:, student: }) }
+
+        it 'retrieves student name and id' do
+          expect(
+            described_class.find_all_students_enrolled_on_year(year).first.first.positive? &&
+            described_class.find_all_students_enrolled_on_year(year).first.second.include?(student.name)
+          ).to be(true)
+        end
+
+        it 'does not retrieves all the other data' do
+          expect(
+            described_class.find_all_students_enrolled_on_year(year).first.third.nil? &&
+            described_class.find_all_students_enrolled_on_year(year).first.fourth.nil? &&
+            described_class.find_all_students_enrolled_on_year(year).first.fifth.nil?
+          ).to be(true)
+        end
+      end
+    end
+
+    describe 'find_student_by_id_enrolled_on_year' do
+      context 'when searching for a student enrolled on a specific year through its id' do
+        let!(:year) { rand(2012..2023) }
+        let!(:course) { create(:course, { year: }) }
+        let!(:student) { create(:student) }
+        let!(:enrollment) { create(:enrollment, { course:, student: }) }
+
+        it 'retrieves the requested student enrolled on a specific year as a Student model object' do
+          expect(
+            described_class.find_student_by_id_enrolled_on_year(year:, id: student.id)
+          ).to eq([student])
+        end
+      end
+
+      context 'when searching for a non student enrolled on a specific year through its id' do
+        let!(:year) { rand(2023) }
+        let!(:course) { create(:course, { year: 2012 }) }
+        let!(:student) { create(:student) }
+        let!(:enrollment) { create(:enrollment, { course:, student: }) }
+
+        it 'retrieves an empty array' do
+          expect(
+            described_class.find_student_by_id_enrolled_on_year(year:, id: student.id)
+          ).to be_empty
+        end
+      end
+    end
+
+    describe 'find_student_grades_averages_by_id' do
+      context 'when retrieving all averages from a student through its id' do
+        let!(:year_5th) { 2022 }
+        let!(:year_6th) { 2023 }
+        let!(:course_5th) { create(:course, { year: year_5th, name: '5th grade' }) }
+        let!(:course_6th) { create(:course, { year: year_6th, name: '6th grade' }) }
+        let!(:student) { create(:student) }
+        let!(:enrollment_5th) { create(:enrollment, { course: course_5th, student: }) }
+        let!(:enrollment_6th) { create(:enrollment, { course: course_6th, student: }) }
+        let!(:subject) { create(:subject) }
+        let!(:exams_5th) { create_list(:exam, 2, { course: course_5th, subject: }) }
+        let!(:exams_6th) { create_list(:exam, 2, { course: course_6th, subject: }) }
+        let!(:grades_5th) do
+          create_list(:grade, 2) do |grade, i|
+            grade.exam = exams_5th[i]
+            grade.enrollment_code = enrollment_5th.code
+          end
+        end
+        let!(:grades_6th) do
+          create_list(:grade, 2) do |grade, i|
+            grade.exam = exams_6th[i]
+            grade.enrollment_code = enrollment_6th.code
+          end
+        end
+
+        let(:average_5th) do
+          grades_values = grades_5th.map { |grade| grade.value }
+          grades_values.sum / grades_values.length
+        end
+        let(:average_6th) do
+          grades_values = grades_6th.map { |grade| grade.value }
+          grades_values.sum / grades_values.length
+        end
+
+        it 'retrieves subject name, course name, course year
+        and the average of the grades grouped by course name and year' do
+          binding.pry
+          expect(described_class.find_student_grades_averages_by_id(student.id)
+          .first.flatten).to eq([subject.name, course.name, course.year, average])
+        end
+      end
+    end
   end
 end
