9bb3b332b95744e1cbfb8c0a4006b652f1739f28
jonathanrib3 <jonathanrib.3@gmail.com>
2023-09-01 11:20:20 -0300

    Refactor controllers to render task 1 data

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,25 @@
 # frozen_string_literal: true
 
 class StudentsController < ApplicationController
-  before_action :set_student, only: %i[show edit update destroy]
+  before_action :set_student, only: %i[edit update destroy]
+  before_action :set_year, only: %i[index show]
 
   def index
-    @students = Student.all
+    @students = Student.find_all_students_enrolled_on_year(@year)
+    render :index, status: :ok
   end
 
-  def show; end
+  def show
+    @student = Student.find_student_by_id_enrolled_on_year(year: @year, id: params[:id]).first
+    if @student.nil?
+      render :not_enrolled, status: :not_found
+    else
+      @enrollment_code =
+        @student.enrollments.find { |enrollment| enrollment.course.year == @year }.code
+      @averages = Student.find_student_grades_averages_by_id(params[:id])
+      render :show, status: :ok
+    end
+  end
 
   def new
     @student = Student.new
@@ -42,6 +54,10 @@ class StudentsController < ApplicationController
 
   def set_student
     @student = Student.find(params[:id])
+  end
+
+  def set_year
+    @year = params.key?(:year) ? params[:year].to_i : 2023
   end
 
   def student_params
